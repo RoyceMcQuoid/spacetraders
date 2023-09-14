678da80924bdc6ae14708ddeb4fdef71b3c83538
Royce McQuoid <royce.mcquoid@7factor.io>
2023-09-14 01:26:54 -0500

    Added header to site and connected it to agent data.

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,42 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div id="app">
+    <Header />
+    <RouterView id="content"/>
+  </div>
 </template>
 
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import  Header from '@/components/Header.vue'
+
+import App from './App.vue'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia';
+
+
+const app = createApp(App)
+app.use(createPinia())
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  position: relative;
+  min-width: 825px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#content {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: stretch;
+  min-height: 100%;
+  flex: 1;
+  margin: 20px;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome Space Traders!</h1>
+    <Login
+      v-if="!authStore.agent"
+      @agentTokenSubmit="getAgent"
+    />
+    <p v-if="message"> {{ message }}</p>
   </main>
 </template>
+
+<script>
+import Login from '@/components/Login.vue'
+import {useAuthStore} from "@/stores/auth.store";
+
+export default {
+  name: 'Home',
+  title: 'StarTraders',
+  components: {Login},
+  methods: {
+    async getAgent(token) {
+      await this.authStore.getAgent(token);
+      console.log(`agent: `, this.authStore.agent);
+    },
+  },
+  data() {
+    return {
+      message: '',
+      authStore: useAuthStore()
+    };
+  },
+}
+</script>
