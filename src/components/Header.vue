74797443ee35694aae1fd4987af4769bc7bab10e
Royce McQuoid <royce.mcquoid@7factor.io>
2023-09-14 01:26:19 -0500

    Created Header component.

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header-div">
+      <h1 style="font-size: xxx-large">Space Traders</h1>
+      <h3 v-if="authStore.agent !== null">Agent: {{this.authStore.agent.symbol}}</h3>
+      <h3 v-if="authStore.agent !== null">Credits: {{this.authStore.agent.credits}}</h3>
+  </div>
+  <hr>
+</template>
+
+<script>
+import {useAuthStore} from "@/stores/auth.store";
+export default {
+  name: 'Header',
+  data() {
+    return {
+      authStore: useAuthStore()
+    };
+  },
+};
+</script>
+
+<style>
+
+.header-div {
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 10px 20px 4px;
+  min-height: 100px;
+}
+</style>
