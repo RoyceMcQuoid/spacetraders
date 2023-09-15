c9104bbc2f824989eb2f675a3175818dee2ae2ac
Royce McQuoid <royce.mcquoid@7factor.io>
2023-09-15 02:03:32 -0500

    Rebuild Header in vue ts format.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="header-div">
       <h1 style="font-size: xxx-large">Space Traders</h1>
-      <h3 v-if="authStore.agent !== null">Agent: {{this.authStore.agent.symbol}}</h3>
-      <h3 v-if="authStore.agent !== null">Credits: {{this.authStore.agent.credits}}</h3>
+      <h3 v-if="authStore.agent !== null">Agent: {{ agentName }}</h3>
+      <h3 v-if="authStore.agent !== null">Credits: {{ credits }}</h3>
   </div>
   <hr>
 </template>
 
-<script>
+<script setup lang="ts">
 import {useAuthStore} from "@/stores/auth.store";
-export default {
-  name: 'Header',
-  data() {
-    return {
-      authStore: useAuthStore()
-    };
-  },
-};
+import {computed} from "vue";
+
+const authStore = useAuthStore();
+
+const agentName = computed<string | null>(() => authStore.agent.symbol);
+const credits = computed<number | null>(() => authStore.agent.credits);
 </script>
 
 <style>
