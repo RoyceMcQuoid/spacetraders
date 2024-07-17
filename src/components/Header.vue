78dee71b09eb544fddbdb17cb6e194948e1b183c
Royce McQuoid <royce.mcquoid@7factor.io>
2024-07-17 17:11:50 -0500

    Store agent key in storage, and re-fetch data on mount if it's empty.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="header-div">
       <h1 style="font-size: xxx-large">Space Traders</h1>
-      <h3 v-if="authStore.agent !== null">Agent: {{ agentName }}</h3>
-      <h3 v-if="authStore.agent !== null">Credits: {{ credits }}</h3>
+      <h3 v-if="agent !== null">Agent: {{ agent.symbol}}</h3>
+      <h3 v-if="agent !== null">Credits: {{ agent.credits}}</h3>
   </div>
   <hr>
 </template>
 
 <script setup lang="ts">
 import {useAuthStore} from "@/stores/auth.store";
-import {computed} from "vue";
+import {computed, onMounted} from "vue";
+import {type Agent} from "@/models/Agent";
 
 const authStore = useAuthStore();
 
-const agentName = computed<string | null>(() => authStore.agent.symbol);
-const credits = computed<number | null>(() => authStore.agent.credits);
+const agent = computed<Agent | null>(() => authStore.agent)
+
+onMounted(async () => {
+  if(agent.value === null) {
+    await authStore.loadAgent();
+  }
+})
+
+
 </script>
 
 <style>
--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia';
 import type {Agent} from "@/models/Agent";
 import type {FactionSymbol} from "@/models/Faction";
 import {API} from "@/api";
+import { useLocalStorage} from "@vueuse/core";
 
 export interface AuthState {
   agentToken: string | null
@@ -12,20 +13,20 @@ export interface AuthState {
 }
 
 export const useAuthStore = defineStore('auth', {
-  state: (): AuthState => ({
-    agentToken: null,
-    agent: null,
-    contracts: null,
-    faction: null,
-    ships: null
-  }),
+  state: () => {
+    return {
+      agentToken: useLocalStorage('agentToken', null),
+      agent: null,
+      contracts: null,
+      faction: null,
+      ships: null,
+    }
+  },
   actions: {
     async loadShips() {
-      console.log('loading ships 1');
       if(this.agentToken) {
         try {
           const result = await API.getShipList(this.agentToken);
-          console.log('result', result);
           if (result.data) {
             this.ships = result.data.data;
           }
@@ -33,7 +34,19 @@ export const useAuthStore = defineStore('auth', {
           console.log(e);
         }
       }
+    },
+    async loadAgent() {
+      if(this.agentToken) {
+        try {
+          const result = await API.getAgent(this.agentToken);
+          if (result.data) {
+            this.agent = result.data.data;
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      }
     }
-  }
+  },
 
 });
